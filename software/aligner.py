daa1a0444902121dc9917bc092e4f1103a0e36e3
Chris Lo <chris.aa.lo@gmail.com>
2024-10-01 12:07:27 -0700

    blob

---
+++ software/aligner.py
@@ -0,0 +1,35 @@
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--reference', '-r', type=str, required=True, help="Reference genome file")
+parser.add_argument('--input', '-i', type=str, required=True, help="Input fastq file")
+
+args = parser.parse_args()
+
+if not os.path.exists(args.input):
+    print("Input file does not exist.")
+    exit()
+
+if not os.path.exists(args.reference):
+    print("Reference file does not exist.")
+    exit()
+
+if not args.input.endswith(".fastq"):
+    print("Expect input to be in fastq format.")
+    exit()
+
+if not args.reference.endswith(".fasta"):
+    print("Expect reference file in fasta format.")
+    exit(0)
+
+with open(args.input) as f:
+    i = 1
+    j = 1
+    for line in f:
+        if i % 4 == 2:
+            print("Aligned sequence ", j, ": ", line.strip())
+            j += 1
+        i += 1
+
+
